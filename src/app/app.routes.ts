74479bd973c11467788e812b77866e1f455acd99
Carlos Chulo <thecompscientist.dev@gmail.com>
2018-02-07 17:33:35 -0800

    completed the event page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent }       from './login/login.component';
 import { HomeComponent }        from './home/home.component';
+import { EventComponent }       from './event/event.component';
 
 // Route Configuration
 export const routes: Routes = [
   { path: "", component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'event', component: EventComponent }
 ];
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit {
   }
 
   searchForEvents() {
+    this.router.navigate(['event']);
   }
 
 }
